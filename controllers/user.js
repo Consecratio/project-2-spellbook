843bccbecbbb72c4725cc75e572cdb617c7588f9
Consecratio <jcasique@gmail.com>
2021-06-09 20:58:44 -0700

    spell list can now interact with backend, need to continue making routes and add spells to db

--- controllers/user.js
+++ controllers/user.js
@@ -3,8 +3,15 @@ const router = express.Router()
 
 // GET /user/:uId -- returns home page for the logged in user of :uId
 router.get('/:uId', (req, res) => {
-    console.log(req.params.uId)
-    res.render('user/index')
+    if(req.params.uId !== 0){
+        res.render('user/index', { user: req.params.uId })
+    }
+})
+
+// POST /user/addSpell -- add spell from /spells/index to user's spellbook
+router.post('/addSpell', (req, res) => {
+    console.log(req.body)
+    res.send(`Added ${req.body.spellName} to Spellbook: ${req.body.spellbookSelect}`)
 })
 
 module.exports = router
