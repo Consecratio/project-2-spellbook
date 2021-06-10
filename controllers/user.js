ea04bb77fe07a2241511f0f53cba865e3cff698d
Consecratio <jcasique@gmail.com>
2021-06-10 10:51:41 -0700

    added ability for user to add new book

--- controllers/user.js
+++ controllers/user.js
@@ -1,11 +1,42 @@
 const express = require('express')
+const db = require('../models')
 const router = express.Router()
 
 // GET /user/:uId -- returns home page for the logged in user of :uId
 router.get('/:uId', (req, res) => {
-    if(req.params.uId !== 0){
-        res.render('user/index', { user: req.params.uId })
-    }
+    let userId = req.params.uId
+
+    // get list of spellbooks owned by user
+    db.spellbook.findAll({
+        where: {
+            userId: userId
+        }
+    }).then(books => {
+        console.log("👋👋👋👋👋👋👋👋👋👋👋👋👋👋👋👋👋👋👋👋")
+        console.log(books)
+        console.log("👋👋👋👋👋👋👋👋👋👋👋👋👋👋👋👋👋👋👋👋")
+        res.render('user/index', {
+            userId: userId,
+            books: books
+        })
+    })
+})
+
+// POST /user/addBook -- add spellbook to user's account
+router.post('/addBook', (req, res) => {
+    let bookName = req.body.bookName
+    let bookDesc = req.body.bookDesc
+    let userId = req.body.userId
+
+    db.spellbook.create({
+        userId: userId,
+        name: bookName,
+        description: bookDesc
+    }).then(book => {
+        res.redirect(`./${userId}`)
+    }).catch(err => {
+        console.log(err)
+    })
 })
 
 // POST /user/addSpell -- add spell from /spells/index to user's spellbook
